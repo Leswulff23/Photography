c5fb8531241fc6885dcf334bec82f3e67b20d830
Leswulff23 <41211344+Leswulff23@users.noreply.github.com>
2021-12-26 10:11:49 +0000

    Updates

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let progressSection = document.querySelector('.progress-section');
+let progressBar = document.querySelector('.progress-bar');
+let progressNum = document.querySelector('.progress-num');
+
+
+let x, y;
+
+function updateProgressBar() {
+    progressBar.style.height = `${getScrollPercent()}%`;
+    progressNum.innerText = `${Math.ceil(getScrollPercent())}%`;
+    requestAnimationFrame(updateProgressBar);
+}
+
+function getScrollPercent() {
+    return ((window.scrollY)/(document.body.scrollHeight-window.innerHeight)*100)
+}
+
+updateProgressBar()
